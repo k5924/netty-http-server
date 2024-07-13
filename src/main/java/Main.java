fc1a28fd7bb5e1f41f22d89956f21f1b1b73a558
Kamran Bhatti <k.bhatti5384@yahoo.co.uk>
2024-07-13 18:47:38 +0100

    use netty to start server

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import io.netty.handler.logging.LogLevel;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -7,19 +9,13 @@ public class Main {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
-    // try {
-    //   ServerSocket serverSocket = new ServerSocket(4221);
-    //
-    //   // Since the tester restarts your program quite often, setting SO_REUSEADDR
-    //   // ensures that we don't run into 'Address already in use' errors
-    //   serverSocket.setReuseAddress(true);
-    //
-    //   serverSocket.accept(); // Wait for connection from client.
-    //   System.out.println("accepted new connection");
-    // } catch (IOException e) {
-    //   System.out.println("IOException: " + e.getMessage());
-    // }
+    final var server = new Server(4221);
+      try {
+          server.start();
+          Runtime.getRuntime().addShutdownHook(new Thread(server::stop));
+      } catch (final Exception e) {
+          System.err.println("Failed to start server due to: " + e.getMessage());
+         server.stop();
+      }
   }
 }
---
+++ src/main/java/Server.java
@@ -0,0 +1,35 @@
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+public final class Server {
+
+    private final int port;
+    private EventLoopGroup bossGroup;
+    private EventLoopGroup workerGroup;
+
+    public Server(final int port) {
+        this.port = port;
+    }
+
+    public void start() throws Exception {
+        bossGroup = new NioEventLoopGroup(1);
+        workerGroup = new NioEventLoopGroup();
+        final var serverBootstrap = new ServerBootstrap();
+        serverBootstrap.option(ChannelOption.SO_BACKLOG, 1024)
+                .group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .handler(new LoggingHandler(LogLevel.INFO));
+        final var channel = serverBootstrap.bind(port).sync().channel();
+        channel.closeFuture().sync();
+    }
+
+    public void stop() {
+        bossGroup.shutdownGracefully();
+        workerGroup.shutdownGracefully();
+    }
+}
