b048745521a73e0c4cd64088fc3b05f30b067735
Kamran Bhatti <k.bhatti5384@yahoo.co.uk>
2024-07-13 22:47:57 +0100

    add to router

--- src/main/java/handlers/RouteHandler.java
+++ src/main/java/handlers/RouteHandler.java
@@ -3,6 +3,7 @@ package handlers;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpMethod;
 
 public final class RouteHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
 
@@ -16,14 +17,24 @@ public final class RouteHandler extends SimpleChannelInboundHandler<FullHttpRequ
     protected void channelRead0(final ChannelHandlerContext channelHandlerContext,
                                 final FullHttpRequest fullHttpRequest) throws Exception {
         final SimpleChannelInboundHandler<FullHttpRequest> handler;
-        if (fullHttpRequest.uri().equals("/")) {
-            handler = new OkResponseHandler();
-        } else if (fullHttpRequest.uri().contains("/echo")) {
-            handler = new EchoHandler();
-        } else if (fullHttpRequest.uri().contains("/user-agent")) {
-            handler = new UserAgentHandler();
-        } else if (fullHttpRequest.uri().contains("/files")) {
-            handler = new FileReadHandler(directory);
+        if (fullHttpRequest.method().equals(HttpMethod.GET)) {
+            if (fullHttpRequest.uri().equals("/")) {
+                handler = new OkResponseHandler();
+            } else if (fullHttpRequest.uri().contains("/echo")) {
+                handler = new EchoHandler();
+            } else if (fullHttpRequest.uri().contains("/user-agent")) {
+                handler = new UserAgentHandler();
+            } else if (fullHttpRequest.uri().contains("/files")) {
+                handler = new FileReadHandler(directory);
+            } else {
+                handler = new NotFoundHandler();
+            }
+        } else if (fullHttpRequest.method().equals(HttpMethod.POST)) {
+            if (fullHttpRequest.uri().contains("/files")) {
+                handler = new FileWriteHandler(directory);
+            } else {
+                handler = new NotFoundHandler();
+            }
         } else {
             handler = new NotFoundHandler();
         }
