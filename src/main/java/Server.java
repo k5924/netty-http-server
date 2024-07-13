47f1d875d81e408d69c7f9b3a19d460c95719364
Kamran Bhatti <k.bhatti5384@yahoo.co.uk>
2024-07-13 18:50:12 +0100

    add channel initializer

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,8 +1,13 @@
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 
@@ -23,7 +28,15 @@ public final class Server {
         serverBootstrap.option(ChannelOption.SO_BACKLOG, 1024)
                 .group(bossGroup, workerGroup)
                 .channel(NioServerSocketChannel.class)
-                .handler(new LoggingHandler(LogLevel.INFO));
+                .handler(new LoggingHandler(LogLevel.INFO))
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(final SocketChannel channel) throws Exception {
+                        final var pipeline = channel.pipeline();
+                        pipeline.addLast(new HttpRequestDecoder());
+                        pipeline.addLast(new HttpResponseEncoder());
+                    }
+                });
         final var channel = serverBootstrap.bind(port).sync().channel();
         channel.closeFuture().sync();
     }
